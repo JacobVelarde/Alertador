08dc4e68909add949c385ed248d4fc5b1351541b
Jacob Velarde <st.jacob1992@gmail.com>
2019-05-11 17:03:32 -0500

    Zoom en la ciudad de Huacho, Perú y se hacen mas grandes los iconos de las alertas

--- js/alertasDao.js
+++ js/alertasDao.js
@@ -27,9 +27,12 @@ $(function() {
 
 function initMap(){
   mapboxgl.accessToken = 'pk.eyJ1IjoiYWxlcnRhZG9ycGVydSIsImEiOiJjanYwazZwNXgxa2IzM3ptZm84em83M21qIn0.kfhR7ywDyyuMxGgdmMEG3A';
+  var huacho = [-77.6231581, -11.1271569];
   map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11'
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: huacho,
+    zoom: 11
   });
   var nav = new mapboxgl.NavigationControl();
   map.addControl(new mapboxgl.FullscreenControl());
